a8dd277abf30e9c33f43cc7ca75d8137a9344579
AmritGoesCrazy <amritpalaujla204@gmail.com>
2023-09-19 15:58:36 -0700

    reversestring complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+newWord = ''
+for (let i = 1; i <= word.length; i++){
+    newWord += word.charAt(word.length-i)
+}
+return newWord
 };
 
 // Do not edit below this line
